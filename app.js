574b08fbe91eea3afc3c83901748eeae18fbdcab
Mark Johnson <markjohnson303@gmail.com>
2019-11-04 12:00:57 -0500

    move api in app

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const http = require("http"),
   server = http.createServer();
 const axios = require("axios");
-const properties = require("./propertiesApi.js");
 
-properties.getProperties();
+const getProperties = () => {
+  axios
+    .get("https://interview.domio.io/properties/")
+    .then(function(response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function(error) {
+      // handle error
+      console.log(error);
+    });
+};
+getProperties();
 
 server.listen(3000, () => {
   console.log("Node server created at port 3000");
