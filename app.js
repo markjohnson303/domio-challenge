4c935991011d34907cbb8057386df1f111ba1708
Mark Johnson <markjohnson303@gmail.com>
2019-11-05 11:37:50 -0500

    create table

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const http = require("http"),
   server = http.createServer();
 const axios = require("axios");
+const sqlite3 = require("sqlite3").verbose();
+
+//connect to sqlite db
+let db = new sqlite3.Database("./db/properties.db", err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Connected to the properties SQlite database.");
+});
+
+//create property history table if it does not exist
+db.run(
+  "CREATE TABLE IF NOT EXISTS property_history(id TEXT, type TEXT, dynamicDisplayPrice REAL, basePrice REAL, dateTime TEXT)"
+);
 
 const getProperties = () => {
   axios
@@ -14,8 +28,9 @@ const getProperties = () => {
       console.log(error);
     });
 };
+getProperties();
 
-setInterval(getProperties, 5000);
+//setInterval(getProperties, 5000);
 
 server.listen(3000, () => {
   console.log("Node server created at port 3000");
